f4cfbc5698f5a95918011d9e59d5661b8e7bd828
Julien Belin <julienbelin002@gmail.com>
2019-06-04 19:58:23 +0200

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+
+//handle screen width for mobile
+$( document ).ready(function() {
+  $("#content-container, #page_vyvomaton_ek1, #_bg__vyvomaton_ek2, #trac__11, #trac__10, #trac__9, #trac__8").css('width', window.innerWidth)
+});
---
+++ vyvomaton.php
@@ -0,0 +1,323 @@
+<?php
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
+	<head>
+		<meta http-equiv="content-type" content="text/html" charset="utf-8" />
+		<title>Je suis un militant du type Transfarmers !</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="" >
+		<link rel="StyleSheet" href="vyvomaton.css" />
+		<meta property="og:title"              content="Je suis un militant du type Hacker Vaillant !" />
+		<meta property="og:description"        content="TODOOO" />
+		<meta property="og:image"              content="<?= 'http://' . $_SERVER['HTTP_HOST'] ."/img/avatar/techno.png"?>"/>
+		<script src="js/jquery.js"></script>
+		<script src="js/custom.js"></script>
+		<script src="js/isotope.js"></script>
+	</head>
+	<body>
+		<div id="content-container" >
+			<div id="page_vyvomaton_ek1"  >
+				<div id="_bg__vyvomaton_ek2"  ></div>
+				<!--<img src="skins/trac__11.png" id="trac__11" />-->
+				<img src="skins/trac__10.png" id="trac__10" />
+				<img src="skins/trac__9.png" id="trac__9" />
+				<img src="skins/trac__8.png" id="trac__8" />
+				<div id="bonjour__hacker_vaillant__" >
+				<span id="bonjour">Bonjour</span>	 <br/>Transfarmers !
+				</div>
+
+				<div id="groupe_5"  >
+
+					<div id="groupe_2"  >
+						<div id="rectangle_2"  ></div>
+
+					</div>
+
+					<div id="groupe_1"  >
+						<div id="agir" >
+							<a href="#__p__agir__">AGIR</a>
+						</div>
+
+					</div>
+
+				</div>
+
+				<div id="groupe_4"  >
+
+					<div id="groupe_3"  >
+						<div id="rectangle_2_ek1"  ></div>
+
+					</div>
+					<div id="mon__profil" >
+						<a href="#__p__mon_profil__hacker_vaillant">MON <br/>PROFIL</a>
+
+					</div>
+
+				</div>
+				<img src="img/avatar/techno.png" id="sans_titre___1" />
+				<div id="__p__mon_profil__hacker_vaillant" >
+					<span style="color:#0000b7; font-style:normal; font-weight:normal; ">Mon profil <br/></span><span style="color:#0000b7; font-size:29px; font-style:normal; font-weight:normal; ">TRANSFARMERS</span>
+				</div>
+				<div id="__p__faire_face_au_bouleversement_num_rique_et_technologique_actuel_est_un_enjeu_majeur_de_notre_g_n_ration_et_des_suivantes__et_cela_vous_anime____ethique__fracture_num_rique_et_inclusion__respect_de_la_vie_priv_e__impact_sur_la_sant___empreinte__cologique_son" >
+					Réussir à nourrir tous les humains tout en préservant notre santé et notre écosystème est sans conteste un enjeu majeur de notre génération et des suivantes, et cela vous anime!
+<br />
+	Répartition équitable des ressources dans le monde, santé, agriculture durable, éthique animale et gaspillage alimentaire, sont autant de sujets au centre de vos préoccupations pour la construction du monde de demain.
+	<br />
+
+	Envie de vous engager plus activement pour un meilleur système alimentaire ? <br /> Voici quelques pistes d’action …
+	<br />
+	L’oignon fait la force !
+				</div>
+				<div id="__p__agir__" >
+					AGIR <br/>
+				</div>
+				<div id="__p__voici_quelques_actions_correspondant___votre_profil_hackeur_vaillant" >
+					<span style="color:#ffffff; font-style:normal; font-weight:normal; ">Voici quelques actions correspondantes à votre profil</span>
+				</div>
+
+				<div id="groupe_11"  >
+
+					<div id="groupe_8"  >
+
+						<div id="groupe_2_ek1"  >
+							<div id="rectangle_2_ek2"  ></div>
+
+						</div>
+
+						<div id="groupe_1_ek1"  >
+							<div id="partager_mon_profil" >
+								<a href="https://www.facebook.com/dialog/share?app_id=2080282928931116&display=popup&href=<?= 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>&redirect_uri=<?= 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>">
+									Partager mon profil
+								</a>
+							</div>
+						</div>
+
+					</div>
+					<img src="skins/rectangle_8.png" id="rectangle_8" />
+
+				</div>
+
+				<div id="groupe_12"  >
+					<div id="rectangle_3"  ></div>
+					<div id="rectangle_4"  ></div>
+
+				</div>
+
+				<div id="groupe_13"  >
+					<div id="rectangle_3_ek1"  ></div>
+					<img src="skins/trac__5.png" id="trac__5" />
+
+				</div>
+				<div id="__p__a_la_maison" >
+					A LA MAISON
+				</div>
+				<div id="__p__en_exterieur" >
+					EN<br/>EXTERIEUR
+				</div>
+				<div id="__p__au__travail" >
+					AU <br/>TRAVAIL
+				</div>
+				<div id="__p___v_nements" >
+					ÉVÉNEMENTS
+				</div>
+				<!--<div id="__p__autres_profils" >
+					AUTRES PROFILS
+				</div>-->
+
+
+				<div id="grille_de_r_p_tition_2"  >
+
+					<div class="grid">
+
+						<div class="element-item maison alimentation" style="position:relative">
+								<img src="img/rectangle_6.png" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+									Cuisiner des produits frais
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+						<div class="element-item maison alimentation" style="position:relative">
+								<img src="img/potager.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+									Faire un potager en permaculture
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+						<div class="element-item maison alimentation" style="position:relative">
+								<img src="img/gaspillage.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Faire la chasse au gaspillage alimentaire
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+						<div class="element-item maison alimentation" style="position:relative">
+								<img src="img/viande.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Réduire ma consommation de viande
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+
+						<div class="element-item exterieur alimentation" style="position:relative">
+								<img src="img/amap.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Faire mes courses à la ferme ou dans une amap
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+
+						<div class="element-item exterieur alimentation" style="position:relative">
+								<img src="img/amap.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Faire mes courses à la ferme ou dans une amap
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+
+						<div class="element-item exterieur alimentation" style="position:relative">
+								<img src="img/label.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Favoriser l’achat de produits labellisés, locaux et de saison
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+						<div class="element-item exterieur alimentation" style="position:relative">
+								<img src="img/invendues.jpg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Acheter des produits invendus (too good to go…)
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+						<div class="element-item exterieur alimentation" style="position:relative">
+								<img src="img/banquealim.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Faire des dons aux banques alimentaires
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+						<div class="element-item exterieur alimentation" style="position:relative">
+								<img src="img/humanitaire.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										M’engager dans une action humanitaire
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+						<div class="element-item exterieur alimentation" style="position:relative">
+								<img src="img/manif.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Soutenir une association militante
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+
+						<div class="element-item travail alimentation" style="position:relative">
+								<img src="img/manif.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Inciter mon entreprise à proposer une restauration durable
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+						<div class="element-item travail alimentation" style="position:relative">
+								<img src="img/restaurant.jpeg" class="rectangle_6" />
+								<div id="rectangle_7"  ></div>
+								<div class="action-text" >
+										Inciter mon entreprise à proposer une restauration durable
+								</div>
+								<div id="ellipse_1"  ></div>
+								<img src="skins/image_1.png" id="image_1" />
+						</div>
+
+					</div>
+
+				</div>
+
+				<!-- <div id="grille_de_r_p_tition_1"  >
+
+					<div id="groupe_18"  >
+						<img src="skins/trac__7_ek1.png" id="trac__7_ek1" />
+						<img src="skins/sans_titre___1_ek2.png" id="sans_titre___1_ek2" />
+						<div id="__p__eco_centrique___ek1" >
+							<span style="color:#0000b7; font-style:normal; font-weight:normal; ">ECO-CENTRIQUE<br/></span><span style="color:#0000b7; font-size:30px; font-style:normal; font-weight:normal; "></span>
+						</div>
+
+					</div>
+
+					<div id="groupe_18_ek1"  >
+						<img src="skins/trac__7_ek2.png" id="trac__7_ek2" />
+						<img src="skins/sans_titre___1_ek3.png" id="sans_titre___1_ek3" />
+						<div id="__p__eco_centrique___ek2" >
+							<span style="color:#0000b7; font-style:normal; font-weight:normal; ">ECO-CENTRIQUE<br/></span><span style="color:#0000b7; font-size:30px; font-style:normal; font-weight:normal; "></span>
+						</div>
+
+					</div>
+
+					<div id="groupe_18_ek2"  >
+						<img src="skins/trac__7_ek3.png" id="trac__7_ek3" />
+						<img src="skins/sans_titre___1_ek4.png" id="sans_titre___1_ek4" />
+						<div id="__p__eco_centrique___ek3" >
+							<span style="color:#0000b7; font-style:normal; font-weight:normal; ">ECO-CENTRIQUE<br/></span><span style="color:#0000b7; font-size:30px; font-style:normal; font-weight:normal; "></span>
+						</div>
+
+					</div>
+
+					<div id="groupe_18_ek3"  >
+						<img src="skins/trac__7_ek4.png" id="trac__7_ek4" />
+						<img src="skins/sans_titre___1_ek5.png" id="sans_titre___1_ek5" />
+						<div id="__p__eco_centrique___ek4" >
+							<span style="color:#0000b7; font-style:normal; font-weight:normal; ">ECO-CENTRIQUE<br/></span><span style="color:#0000b7; font-size:30px; font-style:normal; font-weight:normal; "></span>
+						</div>
+
+					</div>
+
+					<div id="groupe_18_ek4"  >
+						<img src="skins/trac__7_ek5.png" id="trac__7_ek5" />
+						<img src="skins/sans_titre___1_ek6.png" id="sans_titre___1_ek6" />
+						<div id="__p__eco_centrique___ek5" >
+							<span style="color:#0000b7; font-style:normal; font-weight:normal; ">ECO-CENTRIQUE<br/></span><span style="color:#0000b7; font-size:30px; font-style:normal; font-weight:normal; "></span>
+						</div>
+
+					</div>
+
+				</div>
+				<div id="fait_avec__" >
+					Fait avec <span class="char">%E2%9D%A4</span>
+				</div>
+
+				<img src="skins/digital_logo_thecamp.png" id="digital_logo_thecamp" />
+				<img src="skins/image_2.png" id="image_2" />
+
+-->
+			</div>
+		</div>
+		<script>var specialChars = document.querySelectorAll("span.char"); for(var c=0; c<specialChars.length; c++){ specialChars[c].innerHTML = decodeURIComponent(specialChars[c].innerHTML); }</script>
+	</body>
+</html>
